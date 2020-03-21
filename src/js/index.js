b8b88b7f6cbe2b7dab13e00b878611cb082e9471
TheFrost <frost084@gmail.com>
2020-03-21 09:56:58 -0600

    Refactor: reorder index.js file for better structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,40 @@
 import 'regenerator-runtime/runtime'
 import SceneManager from './webgl/SceneManager'
 
+const resources = [
+  {
+    image: 'https://images.unsplash.com/photo-1533661537256-701c0084511d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
+    degrade: Math.random(),
+    seed: Math.random()
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1557939403-1760a0e47505?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
+    degrade: Math.random(),
+    seed: Math.random()
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1564086539698-2c4d0e640d7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
+    degrade: Math.random(),
+    seed: Math.random()
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1564022228519-2f6975e40da8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
+    degrade: Math.random(),
+    seed: Math.random()
+  }
+]
+
+// selectors
 const canvas = document.getElementById('canvas')
+
+// modules
 const sceneManager = new SceneManager({
   canvas,
-  resources: [
-    {
-      image: 'https://images.unsplash.com/photo-1533661537256-701c0084511d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
-      degrade: Math.random(),
-      seed: Math.random()
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1557939403-1760a0e47505?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
-      degrade: Math.random(),
-      seed: Math.random()
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1564086539698-2c4d0e640d7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
-      degrade: Math.random(),
-      seed: Math.random()
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1564022228519-2f6975e40da8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&h=1024',
-      degrade: Math.random(),
-      seed: Math.random()
-    }
-  ]
+  resources
 }, true)
 
+
+// logic
 const resizeCanvas = () => {
   canvas.style.width = '100%'
   canvas.style.height = '100%'
@@ -48,5 +55,6 @@ const render = () => {
   sceneManager.update()
 }
 
+// init
 bindEvents()
 render()
